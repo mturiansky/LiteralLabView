d3e509ebe73dfdd2c57775279b1eee99a3be4a50
Mark Turiansky <mark.turiansky@yahoo.com>
2015-12-23 16:27:08 -0500

    added cleanup options, configuration always run even on help text, need to fix this

--- Server/app.py
+++ Server/app.py
@@ -5,6 +5,8 @@ app.py
 This file is used to run the development server.
 '''
 
+import os
+import atexit
 from argparse import ArgumentParser as AP
 from configuration import APP as app
 from routes.core import home
@@ -15,9 +17,25 @@ def add_arguments(parser):
                         help='disable the debug information', action='store_true')
     parser.add_argument('-p', '--port', type=int, help='set the port',
                         default=5000)
+    parser.add_argument('-c', '--cleanup',
+                        help='delete database and uploads on exit', action='store_true')
+
+def clean_exit():
+    ''' function to cleanup database and uploads on exit '''
+    if os.path.exists(os.path.realpath(app.config['UPLOADS_FOLDER'])):
+        print '[*] Removing uploads folder...'
+        os.rmdir(app.config['UPLOADS_FOLDER'])
+        print '[+] Uploads folder removed'
+
+    if os.path.exists(os.path.realpath(app.config['SQLALCHEMY_DATABASE_URI'][10:])):
+        print '[*] Removing database...'
+        os.remove(app.config['SQLALCHEMY_DATABASE_URI'][10:])
+        print '[+] Database removed'
 
 def main(args):
     ''' main function to run the app '''
+    if args.cleanup:
+        atexit.register(clean_exit)
     app.run(debug=(not args.nodebug), port=args.port)
 
 if __name__ == '__main__':
